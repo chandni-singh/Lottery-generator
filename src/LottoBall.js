f7b09f593b395655d6b327f98243faa038226d0e
Chandni <chandni1141996@gmail.com>
2021-04-04 21:18:48 +0530

    initial files

---
+++ src/LottoBall.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LottoBall extends Component {
+    render() {
+        return (
+            <h1>Hi from lottoBall!</h1>
+        )
+    }
+}
+
+export default LottoBall;
